472ae1e09f6b61659efe96ec926c9ef923d704ef
Mauricio Villegas <maurikiller732@hotmail.com>
2021-10-14 22:44:08 -0400

    exposing methos in api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\TagImageController;
+use App\Http\Controllers\TagController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -42,3 +44,30 @@ Route::post('uploadFile', function (Request $request) {
     return response()->json($data);
 })->name('utils.uploadFile');
 
+
+/****************************************** Image *********************************/
+Route::group(['prefix' => 'Image', 'middleware' => 'guest'], function () {
+    Route::get('/list', [ImageController::class, 'list'])->name('Image.list');
+    Route::get('/index', [ImageController::class, 'index'])->name('Image.index');
+    Route::post('/destroy', [ImageController::class, 'destroy'])->name('Image.destroy');
+    Route::post('/store', [ImageController::class, 'store'])->name('Image.store');
+    Route::get('/show', [ImageController::class, 'show'])->name('Image.show');
+});
+
+
+/****************************************** Tag *********************************/
+Route::group(['prefix' => 'Tag', 'middleware' => 'guest'], function () {
+    Route::get('/list', [TagController::class, 'list'])->name('Tag.list');
+    Route::get('/index', [TagController::class, 'index'])->name('Tag.index');
+    Route::post('/destroy', [TagController::class, 'destroy'])->name('Tag.destroy');
+    Route::post('/store', [TagController::class, 'store'])->name('Tag.store');
+    Route::get('/show', [TagController::class, 'show'])->name('Tag.show');
+});
+
+/****************************************** TagImage *********************************/
+Route::group(['prefix' => 'TagImage', 'middleware' => 'guest'], function () {
+    Route::get('/list', [TagImageController::class, 'list'])->name('TagImage.list');
+    Route::post('/destroy', [TagImageController::class, 'destroy'])->name('TagImage.destroy');
+    Route::post('/store', [TagImageController::class, 'store'])->name('TagImage.store');
+    Route::get('/show', [TagImageController::class, 'show'])->name('TagImage.show');
+});
